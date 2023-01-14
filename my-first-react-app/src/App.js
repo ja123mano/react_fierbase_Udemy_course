5620e78ee3cc739341231c1bc866d7c95d512069
ja123mano <josesantana99@hotmail.com>
2023-01-14 17:12:02 -0600

    Adding sections 3, 4 and progression of 5

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
+import Title from './Components/Title'
+import Modal from './Components/Modal'
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "Practicar js", id:1},
+    {title: "Ver videos de Youtube", id:2},
+    {title: "Estudiar más programación", id:3}
+  ])
+
+  console.log(showModal) 
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id)
+  }
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
+  const subtitle = "All the latest events in Kaiko's mind"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React & Firebase
-        </a>
-      </header>
+      <Title title="Events in your area" subtitle={subtitle} />
+
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide events</button>
+        </div>
+      )}
+
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show events</button>
+        </div>
+      )}
+
+      {showEvents && events.map((event) => (
+        <React.Fragment key={event.id}>
+          <h2>{event.id} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>Delete event</button>
+        </React.Fragment>
+      ))}
+
+      {showModal && <Modal handleClose={handleClose}>
+        <h2>10% Off coupon code!</h2>
+        <p>Use the code KAIKOSAMA at the checkout.</p>
+      </Modal>}
+
+      <div>
+        <button onClick={() => {setShowModal(true)}}>Show Modal</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// TODO: Sección 33 min 1:42
