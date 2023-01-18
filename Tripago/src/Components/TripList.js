d20328dcae95be9902e67d215ac621e2d580003d
ja123mano <josesantana99@hotmail.com>
2023-01-18 11:54:25 -0600

    Finished section 8

--- Tripago/src/Components/TripList.js
+++ Tripago/src/Components/TripList.js
@@ -1,30 +1,34 @@
-import { useState, useEffect, useCallback } from "react"
+import { useState } from "react"
+import { useFetch } from "../hooks/useFetch"
 
 //  styles
 import "./TripList.css"
 
 export default function TripList() {
-    const [trips, setTrips] = useState([])
     const [url, setUrl] = useState("http://localhost:3000/trips")
+    const { data: trips, isPending, error } = useFetch(url)
 
-    const fetchTrips = useCallback(async () => {
-        const response = await fetch(url)
-        const json = await response.json()
-        setTrips(json)
-    }, [url])
+    // const fetchTrips = useCallback(async () => {
+    //     const response = await fetch(url)
+    //     const json = await response.json()
+    //     setTrips(json)
+    // }, [url])
 
-    useEffect(() => {
-        fetchTrips()
-    }, [fetchTrips])
+    // useEffect(() => {
+    //     fetchTrips()
+    // }, [fetchTrips])
 
-    console.log(trips)
+    // console.log(trips)
 
     return (
         <div className="trip-list">
             <h2>Trip list</h2>
+
+            {isPending && <div>Loading trips...</div>}
+            {error && <div>{error}</div>}
             
             <ul>
-                {trips.map(trip => (
+                {trips && trips.map(trip => (
                     <li key={trip.id}>
                         <h3>{trip.title}</h3>
                         <p>{trip.loc} - {trip.price}</p>
